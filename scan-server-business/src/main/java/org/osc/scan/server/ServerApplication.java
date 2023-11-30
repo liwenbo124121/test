2fd5d90e54f1a92c697bfdcbf2939e3cf105129d
li <17600881502@163.com>
2023-11-30 21:17:37 +0800

    测试

--- scan-server-business/src/main/java/org/osc/scan/server/ServerApplication.java
+++ scan-server-business/src/main/java/org/osc/scan/server/ServerApplication.java
@@ -22,4 +22,10 @@ public class ServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
+public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
 }
